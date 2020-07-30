8f26e5fde9337306f503c508ded70b9cba697b9d
Krissy Do <krissytdo@hotmail.com>
2020-07-29 22:39:10 -0700

    layout ok calendar is in position and sticks

--- client/src/components/BelowSticky.jsx
+++ client/src/components/BelowSticky.jsx
@@ -1,22 +1,62 @@
-import React from 'react';
-import { Container, Grid } from 'semantic-ui-react';
+import React, {Component, createRef} from 'react';
+import { Container, Grid, Rail, Ref, Sticky, Segment } from 'semantic-ui-react';
 
-const BelowSticky = () =>  (
-  <Container style={{borderBottom: '1px solid #c6c6c6', justifyContent: 'center'}} >
-  <Grid col={1} style={{ paddingTop: '24px', paddingBottom: '0px', justifyContent: 'center'}}>
-    <Grid.Row id={'photos'} style={{padding: '0px'}}/>
-    <Grid.Row id={'photo-modal'} style={{padding: '0px'}}/>
-  </Grid >
-  <Grid>
-    <Grid.Column width={9}>
-      <Grid.Row id={'description'}/>
-      <Grid.Row id={'description-modal-root'}/>
-      <Grid.Row id={'amenities'}/>
-      <Grid.Row id={'calendar'}/>
-    </Grid.Column>
-    <Grid.Column width={7}/>
-  </Grid>
-  </Container>
-)
+class BelowSticky extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.contextRef = createRef();
+    this.handleScroll = this.handleScroll.bind(this);
+  }
+
+  handleScroll(e) {
+    let element = e.target;
+    console.log(element);
+    if (element.scrollHeight - element.scrollTop === element.clientHeight) {
+      this.props.scroll();
+    }
+  }
+
+  render() {
+    return(
+      <Container style={{borderBottom: '1px solid #c6c6c6', justifyContent: 'center', paddingBottom: '48px'}} >
+        <Grid style={{borderBottom: '1px solid #c6c6c6', paddingBottom: '48px'}}>
+          <Grid.Column width={10}>
+            {/* <Container ref={this.contextRef}> */}
+              <Ref innerRef={this.contextRef}>
+                <div style={{border: 'none', paddingTop: '48px'}}>
+              <Grid.Row id={'description'}/>
+              <Grid.Row id={'description-modal-root'}/>
+              <Grid.Row id={'amenities'}/>
+              <Grid.Row id={'reservation-calendar'}/>
+                <Rail position='right'style={{paddingTop: '68px', zIndex: '20'}}>
+              <Sticky context={this.contextRef} offset={100}>
+                  <div id={'reservation'}/>
+                  <div id={'reservation-widget'} style={{backgroundColor: 'rgb(255,255,255)', position: 'relative'}}/>
+              </Sticky>
+                </Rail>
+              </div>
+              </Ref>
+            {/* </Container> */}
+          </Grid.Column>
+          <Grid.Column width={6}/>
+          {/* <Grid.Row id={'reservation'}/>
+          <Grid.Row id={'reservation-widget'}/> */}
+        </Grid>
+      {/* </Container> */}
+            {/* //  <Container style={{borderBottom: '1px solid #c6c6c6', justifyContent: 'center'}} > */}
+             <Grid col={1} style={{borderBottom: '1px solid #c6c6c6', justifyContent: 'center'}} >
+               <Grid.Row style={{borderBottom: '1px solid #c6c6c6', padding: '48px 0px 48px 0px'}} id={'review'}/>
+               <Grid.Row id='reviewmodal-root' style={{padding: '0px'}}/>
+               <Grid.Row style={{borderBottom: '1px solid #c6c6c6', padding: '48px 0px'}} id={'location'}/>
+               <Grid.Row style={{borderBottom: '1px solid #c6c6c6', padding: '48px 0px'}} id={'host'}/>
+               <Grid.Row  id={'thingsToKnow'} style={{padding: '48px 0px'}}/>
+               <Grid.Row id={'suggestions'}/>
+               <Grid.Row id={'things-to-do-carousel'}/>
+             </Grid>
+         </Container>
+    )
+  }
+}
 
 export default BelowSticky;
--- client/src/components/StickyNav.jsx
+++ client/src/components/StickyNav.jsx
@@ -1,13 +1,17 @@
 import React, {Component, createRef} from 'react';
-import ReactDOM from 'react-dom';
 import { Sticky, Menu, Segment, Grid } from 'semantic-ui-react';
 import BelowSticky from './BelowSticky.jsx';
 
 const menuStyle = {
   padding: '0px 40px',
   borderTop: 'none',
-  backgroundColor: '#fff'
+  backgroundColor: '#fff',
+  zIndex: '50'
 };
+
+const hidden = {
+  display: 'none'
+}
 
 const itemStyle={
   paddingTop: '30px',
@@ -26,52 +30,39 @@ class StickyNav extends React.Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      showNav: false
+    };
     this.contextRef = React.createRef();
+    this.scroll = this.scroll.bind(this);
+
+
+  }
+
+  scroll(){
+    this.setState({
+      showNav: true
+    });
+   
   }
 
   render(){
     return (
       <div ref={this.contextRef}>
         <Sticky context={this.contextRef} style={{backgroundColor: '#fff'}}>
+        {/* <Sticky context={this.contextRef} style={{backgroundColor: '#fff', display: this.state.showNav ?'inline' : 'none'}}> */}
           <Menu attached='top' pointing secondary style={menuStyle}>
+          {/* <Menu attached='top' pointing secondary style={{padding: '0px 40px', borderTop: 'none', backgroundColor: '#fff', display: this.state.showNav ?'none' : 'inline'}}> */}
           <Menu.Item href='#photos' style={itemStyle} onMouseEnter={displayBottomBorder} onMouseLeave={noBorder}>Photos</Menu.Item>
           <Menu.Item href='#amenities' style={itemStyle} onMouseEnter={displayBottomBorder} onMouseLeave={noBorder} >Amenities</Menu.Item>
           <Menu.Item href='#review' style={itemStyle} onMouseEnter={displayBottomBorder} onMouseLeave={noBorder}>Reviews</Menu.Item>
           <Menu.Item href='#location' style={itemStyle} onMouseEnter={displayBottomBorder} onMouseLeave={noBorder}>Location</Menu.Item>
           </Menu>
-          </Sticky>
-        <BelowSticky attached='attached'/>
-          {/* </Segment> */}
-   
+        </Sticky>
+        <BelowSticky scroll={this.scroll}/>
         </div>)
       }
 
-
-
-
-  //     ReactDOM.createPortal(
-  // <div ref={this.contextRef}>
-  //   <Sticky context={this.contextRef} style={{backgroundColor: '#fff'}}>
-  // <Menu attached='top' pointing secondary style={menuStyle}>
-  // <Menu.Item href='#photos' style={itemStyle} onMouseEnter={displayBottomBorder} onMouseLeave={noBorder}>Photos</Menu.Item>
-  // <Menu.Item href='#amenities' style={itemStyle} onMouseEnter={displayBottomBorder} onMouseLeave={noBorder} >Amenities</Menu.Item>
-  // <Menu.Item href='#review' style={itemStyle} onMouseEnter={displayBottomBorder} onMouseLeave={noBorder}>Reviews</Menu.Item>
-  // <Menu.Item href='#location' style={itemStyle} onMouseEnter={displayBottomBorder} onMouseLeave={noBorder}>Location</Menu.Item>
-  // </Menu>
-  // </Sticky>
-  // {/* <Grid attached='bottom' style={{borderBottom: '1px solid black'}}>
-  //   {/* <Grid.Row  id={'reservation'}/> */}
-  //       {/* <Grid.Colunn floated='right' id={'reservation'}/> */}
-  //       {/* <Grid.Column floated='left'>
-  //         <Grid.Row  id={'description'}/>
-  //         <Grid.Row  id={'description-modal-root'}/>
-  //       </Grid.Column>
-  //   <Grid.Row  id={'amenities'}/> */}
-  // {/* </Grid> */} */}
-  // </div>,
-  // document.getElementById('nav'))
-  //   );
-  // }
     
 }
 
